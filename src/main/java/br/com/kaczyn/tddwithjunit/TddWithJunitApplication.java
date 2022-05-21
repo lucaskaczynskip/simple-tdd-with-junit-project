c10638968b25bee0bac1761c28fae8315d30490e
Lucas Primon <lucaskaczyn@gmail.com>
2022-05-21 14:33:22 -0300

    commit inicial

---
+++ src/main/java/br/com/kaczyn/tddwithjunit/TddWithJunitApplication.java
@@ -0,0 +1,13 @@
+package br.com.kaczyn.tddwithjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddWithJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddWithJunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kaczyn/tddwithjunit/TddWithJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kaczyn.tddwithjunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddWithJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
